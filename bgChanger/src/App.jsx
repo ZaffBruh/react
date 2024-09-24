1de286643897212be1f3d7ac1ddbfe9e2910f185
zarif007008 <zarif007008az@gmail.com>
2024-09-24 20:09:50 +0600

    set olive color

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -1,11 +1,20 @@
+import React from 'react'
 import { useState } from "react"
 
 
 function App() {
-  const [color, setColor] = useState()
+  const change = () => {
+
+    // const [color, setColor] = useState("olive")
+    document.body.style.backgroundColor = "olive"
+
+  }
+  
+
   return (
     <>
-      
+      {/* <div className="w-full h-screen duration-200"></div> */}
+      <button onClick={change}>Olive</button>
     </>
   )
 }
