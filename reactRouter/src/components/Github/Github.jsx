4896b938c9979c2a6493a6c0502f8e049ab2a22a
zarif007008 <zarif007008az@gmail.com>
2024-10-08 00:29:05 +0600

    useLoaderData

--- reactRouter/src/components/Github/Github.jsx
+++ reactRouter/src/components/Github/Github.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { useLoaderData } from "react-router-dom"
 
 function Github() {
+    const data = useLoaderData()
+    // const [data,setData] = useState([])
+    // useEffect(() => {
+    //     fetch("https://api.github.com/users/ZaffBruh")
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log(data);
+    //         setData(data)
+    //     })
+    // },[])
   return (
-    <div className='bg-gray-800 text-center text-white p-4 m-4 text-xl'>Github followers: </div>
+    <div 
+    className='bg-gray-800 text-center text-white p-4 m-4 text-xl'
+    >
+    Github followers: {data.followers} <br />
+    Github following: {data.following} <br />
+    <img src={data.avatar_url} alt="git picture" width={300}/>
+    </div>
   )
 }
 
 export default Github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch("https://api.github.com/users/ZaffBruh")
+    return response.json()
+}
