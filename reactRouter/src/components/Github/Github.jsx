4a532239b83919c65cf163541744c6b1b75c0eea
zarif007008 <zarif007008az@gmail.com>
2024-10-07 23:58:57 +0600

    creating github component

---
+++ reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Github() {
+  return (
+    <div className='bg-gray-800 text-center text-white p-4 m-4 text-xl'>Github followers: </div>
+  )
+}
+
+export default Github
