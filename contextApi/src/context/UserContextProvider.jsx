ce09f17626e62a5cd5e62a3ad674568b65ec99dc
zarif007008 <zarif007008az@gmail.com>
2024-10-15 01:30:01 +0600

    UserContext.Provider

--- contextApi/src/context/UserContextProvider.jsx
+++ contextApi/src/context/UserContextProvider.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import UserContext from "./UserContext";
 
-const UserContextProvider = () => {
-
+const UserContextProvider = ({children}) => {
+    return(
+        <UserContext.Provider>
+         {children}
+        </UserContext.Provider>
+    )
 }
 
 export default UserContextProvider
