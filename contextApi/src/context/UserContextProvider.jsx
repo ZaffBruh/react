b6de883c26f37602848b9976a70c6e633777ee9b
zarif007008 <zarif007008az@gmail.com>
2024-10-15 23:48:33 +0600

    use []-null argument

--- contextApi/src/context/UserContextProvider.jsx
+++ contextApi/src/context/UserContextProvider.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { useState } from "react";
 import UserContext from "./UserContext";
 
 const UserContextProvider = ({children}) => {
+    const [user, setUser] = React.useState([])
     return(
-        <UserContext.Provider>
+        <UserContext.Provider value={{user, setUser}}>
          {children}
         </UserContext.Provider>
     )
