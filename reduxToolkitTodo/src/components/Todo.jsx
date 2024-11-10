f3d04a44d6ac9db735019f4b14ad97feb6e1bc32
zarif007008 <zarif007008az@gmail.com>
2024-11-11 00:30:18 +0600

    Todo component in progress

--- reduxToolkitTodo/src/components/Todo.jsx
+++ reduxToolkitTodo/src/components/Todo.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { useSelector, UseSelector } from 'react-redux'
-import { UseDispatch } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { removeTodo } from '../features/todo/todoSlice'
 
 function Todo() {
     useSelector(state => state.todos)
+    const dispatch = useDispatch()
+
   return (
-    <div>Todo</div>
+    <>
+     <div>
+      Todos
+     </div>
+    </>
   )
 }
 
